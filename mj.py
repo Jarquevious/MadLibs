46ac0254a07f837cda1bf7b978a191606c87728d
Jarquevious <jarquevious@gmail.com>
2019-09-02 20:45:32 -0700

    Added Variables

--- mj.py
+++ mj.py
@@ -1,10 +1,42 @@
+# FIRST SENTENCE
+noun1 = input("Enter a noun: ")
+
+# SECOND SENTENCE 
+adjective1 = input("Enter a color: ")
+adjective2 = input("Enter a color: ")
+conjection1 = input("Enter a color: ")
+adjective3 = input("Enter a color: ")
+
+# THIRD SENTENCE 
+adjective4 = input("Enter a color: ")
+adjective5 = input("Enter a color: ")
+adverb1 = input("Enter a color: ")
+verb1 = input("Enter a color: ")
+
+#FOURTH SENTENCE 
+pronoun1 = input("Enter a color: ")
+adjective6 = input("Enter a color: ")
+adjective7 = input("Enter a color: ")
+adjective8 = input("Enter a color: ")
+
+# FIFTH SENTENCE 
+adjective9 = input("Enter a color: ")
+
+# SIXTH SENTENCE
+noun2 = input("Enter a color: ")
+adjective10 = input("Enter a color: ")
+
+
+
+
+
 #OUTPUT FUNCTION
 def output():
     print("Micheal Jackson was born in (" + noun1 + ") USA in 1958.") 
     print("He was a (" + adjective1 + ") performer with a (" + adjective2 + ") voice (" + conjection1 + ") a (" + adjective3 + ") personality.")
     print("Although he was a (" + adjective4 + ") person, his family and friends thought he was (" + adjective5 + ") for (" + adverb1 + ") (" + verb1 + ") when he interacted with people.")
-    print("(" + pronoun1 + ") became famous for creating (" + adjective6 + ") music, (" + adjective7 + ") collaborations, and ((" + adjective8 + ") love for his fans. In 1984, Jackson was 
-    print("(" + adjective2 + ") onto the Hollywood Walk of Fame.")
-    print("After his death in 2009, Jackson’s (" + noun2 + ") was released for his fans, becoming the most ((" + adjective9 + ") documentary or concert film ever.")
+    print("(" + pronoun1 + ") became famous for creating (" + adjective6 + ") music, (" + adjective7 + ") collaborations, and (" + adjective8 + ") love for his fans. In 1984, Jackson was")
+    print("(" + adjective9 + ") onto the Hollywood Walk of Fame.")
+    print("After his death in 2009, Jackson’s (" + noun2 + ") was released for his fans, becoming the most ((" + adjective10 + ") documentary or concert film ever.")
 
 
