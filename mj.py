cd9b586554e365765b7c2f40f6b9a9d6f24dad81
Jarquevious <jarquevious@gmail.com>
2019-09-02 20:18:30 -0700

    First commit

---
+++ mj.py
@@ -0,0 +1,10 @@
+#OUTPUT FUNCTION
+def output():
+    print("Micheal Jackson was born in (" + noun1 + ") USA in 1958.") 
+    print("He was a (" + adjective1 + ") performer with a (" + adjective2 + ") voice (" + conjection1 + ") a (" + adjective3 + ") personality.")
+    print("Although he was a (" + adjective4 + ") person, his family and friends thought he was (" + adjective5 + ") for (" + adverb1 + ") (" + verb1 + ") when he interacted with people.")
+    print("(" + pronoun1 + ") became famous for creating (" + adjective6 + ") music, (" + adjective7 + ") collaborations, and ((" + adjective8 + ") love for his fans. In 1984, Jackson was 
+    print("(" + adjective2 + ") onto the Hollywood Walk of Fame.")
+    print("After his death in 2009, Jackson’s (" + noun2 + ") was released for his fans, becoming the most ((" + adjective9 + ") documentary or concert film ever.")
+
+
