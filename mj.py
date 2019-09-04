3ad310d70ff6d5fb715f033c78843274c8449ee2
Jarquevious <jarquevious@gmail.com>
2019-09-03 20:10:46 -0700

    Color added not complete

--- mj.py
+++ mj.py
@@ -1,5 +1,7 @@
 # FIRST SENTENCE
 #Shoutout to Francis my classmate for teaching me the while loop to handle improper user input
+from colorama import init, Fore, Back, Style
+
 noun1 = input("Enter a noun(City in the Midwest USA): ")
 while len(noun1) == 0:
     noun1 = input("Enter again: ")
@@ -72,16 +74,17 @@ while len(adjective10) == 0:
 
 
 
-
 #OUTPUT FUNCTION
 def output():
-    print("Micheal Jackson was born in (" + noun1 + ") USA in 1958.") 
-    print("He was a (" + adjective1 + ") performer with a (" + adjective2 + ") voice (" + conjection1 + ") a (" + adjective3 + ") personality.")
+    print(Back.RED)
+    print("Micheal Jackson was born in " + Back.GREEN + noun1 + Fore.WHITE + "USA in 1958.") 
+    print("He was a" + Fore.GREEN + adjective1 + Fore.WHITE + "performer with a" + Fore.GREEN + adjective2 + Fore.WHITE + "voice" + Fore.GREEN + conjection1 + Fore.WHITE + "a" + Fore.GREEN + adjective3 + Fore.WHITE + "personality.")
     print("Although he was a (" + adjective4 + ") person, his family and friends thought he was (" + adjective5 + ") for (" + adverb1 + ") (" + verb1 + ") when he interacted with people.")
     print("(" + pronoun1 + ") became famous for creating (" + adjective6 + ") music, (" + adjective7 + ") collaborations, and (" + adjective8 + ") love for his fans. In 1984, Jackson was")
     print("(" + adjective9 + ") onto the Hollywood Walk of Fame.")
     print("After his death in 2009, Jackson’s (" + noun2 + ") was released for his fans, becoming the most ((" + adjective10 + ") documentary or concert film ever.")
 
 output() 
+
 
 
